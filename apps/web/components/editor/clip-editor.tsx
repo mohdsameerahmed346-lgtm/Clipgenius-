6fcd5bece4e7c5bb4944b7b164aff46e759ccc59
mohdsameerahmed346-lgtm <mohdsameerahmed346@gmail.com>
2026-05-14 06:10:25 +0530

    Add ClipEditor component for editing clips

---
+++ apps/web/components/editor/clip-editor.tsx
@@ -0,0 +1,9 @@
+export default function ClipEditor() {
+  return (
+    <div className="rounded-2xl bg-zinc-900 p-6">
+      <h2 className="text-xl font-semibold text-white">
+        Clip Editor
+      </h2>
+    </div>
+  )
+}
